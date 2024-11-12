5d5cb04e7259df8b4f274f3bf6be48e0bc9dbd33
huynq <godwu69@gmail.com>
2024-11-12 10:59:32 +0700

    update

--- ComicSystemWeb/src/app/dashboard/dashboard.component.ts
+++ ComicSystemWeb/src/app/dashboard/dashboard.component.ts
@@ -1,71 +1,107 @@
-import {Component, OnInit} from '@angular/core';
-import {Router, RouterOutlet} from '@angular/router';
-import {HttpClient} from '@angular/common/http';
-import {NgForOf} from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import {NgForOf, NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [
-    NgForOf
-  ],
+  imports: [NgForOf, FormsModule, NgIf],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  constructor(private router: Router, private http: HttpClient) { }
-
   books: any[] = [];
+  currentBook: any = {};
+  isEditing: boolean = false;
 
+  constructor(private router: Router, private http: HttpClient) {}
 
-  getAllBooks(){
+  getAllBooks() {
     this.http.get<any>('http://localhost:5235/api/comic').subscribe({
-      next: res => {
+      next: (res) => {
         this.books = res.data;
       },
-      error: err => {
+      error: (err) => {
         console.log(err);
-      }
+      },
     });
   }
 
   viewBook(bookId: number) {
     this.http.get<any>(`http://localhost:5235/api/comic/${bookId}`).subscribe({
-      next: res => {
+      next: (res) => {
         console.log(res);
       },
-      error: err => {
+      error: (err) => {
         console.log(err);
-      }
+      },
     });
   }
 
+  // Edit book
   editBook(bookId: number) {
-    this.http.put<any>(`http://localhost:5235/api/comic/${bookId}`,
-      {}
-    ).subscribe({
-      next: res => {
-        console.log(res);
+    this.isEditing = true;
+    this.http.get<any>(`http://localhost:5235/api/comic/${bookId}`).subscribe({
+      next: (res) => {
+        this.currentBook = res.data;
       },
-      error: err => {
+      error: (err) => {
         console.log(err);
-      }
+      },
     });
   }
 
+  saveBook() {
+    if (this.isEditing) {
+      this.http
+        .put<any>(`http://localhost:5235/api/comic/${this.currentBook.comicBookId}`, this.currentBook)
+        .subscribe({
+          next: (res) => {
+            console.log(res);
+            this.getAllBooks();
+            this.resetForm();
+          },
+          error: (err) => {
+            console.log(err);
+          },
+        });
+    } else {
+      this.http
+        .post<any>('http://localhost:5235/api/comic', this.currentBook)
+        .subscribe({
+          next: (res) => {
+            console.log(res);
+            this.getAllBooks();
+            this.resetForm();
+          },
+          error: (err) => {
+            console.log(err);
+          },
+        });
+    }
+  }
+
+  // Delete book
   deleteBook(bookId: number) {
     this.http.delete<any>(`http://localhost:5235/api/comic/${bookId}`).subscribe({
-      next: res => {
+      next: (res) => {
         console.log(res);
+        this.getAllBooks();
       },
-      error: err => {
+      error: (err) => {
         console.log(err);
-      }
+      },
     });
+  }
+
+  resetForm() {
+    this.isEditing = false;
+    this.currentBook = {};
   }
 
   ngOnInit(): void {
     this.getAllBooks();
   }
-
 }
